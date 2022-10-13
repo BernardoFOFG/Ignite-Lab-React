f66cf3725a7cd0b89f99a7e2b9eeb86e070a224b
BernardoFOFG <bernardofofg@gmail.com>
2022-10-13 14:09:13 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <div className="flex w-full h-full justify-center items-center">
+      <h1 className=" font-bold text-2xl text-black">Déborah, eu te amo</h1>
+    </div>
+  );
+}
