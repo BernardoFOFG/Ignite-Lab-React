e92539ac9ffd3b300fd802e16c02625a55bdf53f
BernardoFOFG <bernardofofg@gmail.com>
2022-10-14 14:28:00 -0300

    adding: tests using new tools

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,17 +1,22 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
   ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-vite",
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite",
   },
-  features: {
-    storyStoreV7: true,
+  "features": {
+    'storyStoreV7': true,
+    'interactionsDebugger': true,
   },
+  "staticDirs": [
+    "../public"
+  ],
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
       config.base = '/Ignite-Lab-React/'
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import { SignIn } from "./pages/SignIn";
 import "./styles/global.css";
+
 
 export function App() {
   return (
-    <div className="flex w-full h-full justify-center items-center">
-      <h1 className=" font-bold text-2xl text-black">Déborah, eu te amo</h1>
-    </div>
-  );
+    <SignIn/>
+  )
 }
